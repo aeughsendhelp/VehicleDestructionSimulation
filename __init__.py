5de848ef9cda65e8aeda3e16ef51110796b1b5a7
EvilPunkin <82129788+EvilPunkin@users.noreply.github.com>
2023-06-25 22:46:09 -0700

    New naming and general cleaning up of things very small commit but I need sleep

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
+# ORGANIZATION
+
+# Operators
+
+# DRAWING
+# Rig
+# Options
+
+# 
+# 
+
 bl_info = {
     "name": "Physics-Based Vehicles",
     "author": "aeugh",
@@ -17,13 +28,13 @@ from mathutils import Vector
 from bpy_extras.object_utils import AddObjectHelper, object_data_add
 from bpy.utils import register_class, unregister_class
 
-class VDP_OT_actions(Operator):
+class VDS_OT_actions(Operator):
     """Move items up and down, add and remove"""
-    bl_idname = "vdp.list_action"
+    bl_idname = "vds.list_action"
     bl_label = "List Actions"
     bl_description = "Move items up and down, add and remove"
     bl_options = {'REGISTER'}
-
+        
     action: bpy.props.EnumProperty(
         items=(
             ('UP', "Up", ""),
@@ -33,59 +44,114 @@ class VDP_OT_actions(Operator):
 
     def invoke(self, context, event):
         scn = context.scene
-        idx = scn.vdp_index
+        idx = scn.vds_index
 
         try:
-            item = scn.vdp[idx]
+            item = scn.vds[idx]
         except IndexError:
             pass
         else:
-            if self.action == 'DOWN' and idx < len(scn.vdp) - 1:
-                item_next = scn.vdp[idx+1].name
-                scn.vdp.move(idx, idx+1)
-                scn.vdp_index += 1
-                info = 'Item "%s" moved to position %d' % (item.name, scn.vdp_index + 1)
+            if self.action == 'DOWN' and idx < len(scn.vds) - 1:
+                item_next = scn.vds[idx+1].name
+                scn.vds.move(idx, idx+1)
+                scn.vds_index += 1
+                info = 'Item "%s" moved to position %d' % (item.name, scn.vds_index + 1)
                 self.report({'INFO'}, info)
 
             elif self.action == 'UP' and idx >= 1:
-                item_prev = scn.vdp[idx-1].name
-                scn.vdp.move(idx, idx-1)
-                scn.vdp_index -= 1
-                info = 'Item "%s" moved to position %d' % (item.name, scn.vdp_index + 1)
+                item_prev = scn.vds[idx-1].name
+                scn.vds.move(idx, idx-1)
+                scn.vds_index -= 1
+                info = 'Item "%s" moved to position %d' % (item.name, scn.vds_index + 1)
                 self.report({'INFO'}, info)
 
             elif self.action == 'REMOVE':
-                info = 'Item "%s" removed from list' % (scn.vdp[idx].name)
-                scn.vdp_index -= 1
-                scn.vdp.remove(idx)
+                info = 'Item "%s" removed from list' % (scn.vds[idx].name)
+                scn.vds_index -= 1
+                scn.vds.remove(idx)
                 self.report({'INFO'}, info)
-
+                    
         if self.action == 'ADD':
             if context.object:
-                item = scn.vdp.add()
+                item = scn.vds.add()
                 item.name = context.object.name
-                item.obj_type = context.object.type
-                item.obj_id = len(scn.vdp)
-                scn.vdp_index = len(scn.vdp)-1
+                item.obj = context.object
+                scn.vds_index = len(scn.vds)-1
                 info = '"%s" added to list' % (item.name)
                 self.report({'INFO'}, info)
             else:
                 self.report({'INFO'}, "Nothing selected in the Viewport")
         return {"FINISHED"}
 
+# class VDS_OT_actions(Operator):
+#     """Move items up and down, add and remove"""
+#     bl_idname = "vds.list_action"
+#     bl_label = "List Actions"
+#     bl_description = "Move items up and down, add and remove"
+#     bl_options = {'REGISTER'}
+
+#     action: bpy.props.EnumProperty(
+#         items=(
+#             ('UP', "Up", ""),
+#             ('DOWN', "Down", ""),
+#             ('REMOVE', "Remove", ""),
+#             ('ADD', "Add", "")))
+
+#     def invoke(self, context, event):
+#         scn = context.scene
+#         idx = scn.vds_index
+
+#         try:
+#             item = scn.vds[idx]
+#         except IndexError:
+#             pass
+#         else:
+#             if self.action == 'DOWN' and idx < len(scn.vds) - 1:
+#                 item_next = scn.vds[idx+1].name
+#                 scn.vds.move(idx, idx+1)
+#                 scn.vds_index += 1
+#                 info = 'Item "%s" moved to position %d' % (item.name, scn.vds_index + 1)
+#                 self.report({'INFO'}, info)
+
+#             elif self.action == 'UP' and idx >= 1:
+#                 item_prev = scn.vds[idx-1].name
+#                 scn.vds.move(idx, idx-1)
+#                 scn.vds_index -= 1
+#                 info = 'Item "%s" moved to position %d' % (item.name, scn.vds_index + 1)
+#                 self.report({'INFO'}, info)
+
+#             elif self.action == 'REMOVE':
+#                 info = 'Item "%s" removed from list' % (scn.vds[idx].name)
+#                 scn.vds_index -= 1
+#                 scn.vds.remove(idx)
+#                 self.report({'INFO'}, info)
+
+#         if self.action == 'ADD':
+#             if context.object:
+#                 item = scn.vds.add()
+#                 item.name = context.object.name
+#                 item.obj_type = context.object.type
+#                 item.obj_id = len(scn.vds)
+#                 scn.vds_index = len(scn.vds)-1
+#                 info = '"%s" added to list' % (item.name)
+#                 self.report({'INFO'}, info)
+#             else:
+#                 self.report({'INFO'}, "Nothing selected in the Viewport")
+#         return {"FINISHED"}
+
     @classmethod
     def poll(cls, context):
-        return bool(context.scene.vdp)
+        return bool(context.scene.vds)
 
     def execute(self, context):
         scn = context.scene
         removed_items = []
         # Reverse the list before removing the items
         for i in self.find_duplicates(context)[::-1]:
-            scn.vdp.remove(i)
+            scn.vds.remove(i)
             removed_items.append(i)
         if removed_items:
-            scn.vdp_index = len(scn.vdp)-1
+            scn.vds_index = len(scn.vds)-1
             info = ', '.join(map(str, removed_items))
             self.report({'INFO'}, "Removed indices: %s" % (info))
         else:
@@ -100,24 +166,28 @@ class VDP_OT_actions(Operator):
 #   Drawing
 # -------------------------------------------------------------------
 
-class VDP_UL_wheels(UIList):
+
+# RIG MENU
+# UI List of all the assigned wheels
+class VDS_UL_wheels(UIList):
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
         split = layout.split(factor=0.3)
         split.label(text="Index: %d" % (index))
-        vdp_icon = "OUTLINER_OB_%s" % item.obj_type
-        #split.prop(item, "name", text="", emboss=False, translate=False, icon=vdp_icon)
-        split.label(text=item.name, icon=vdp_icon) # avoids renaming the item by accident
+        vds_icon = "OUTLINER_OB_%s" % item.obj_type
+        #split.prop(item, "name", text="", emboss=False, translate=False, icon=vds_icon)
+        split.label(text=item.name, icon=vds_icon) # avoids renaming the item by accident
 
     def invoke(self, context, event):
         pass   
 
-class VDP_PT_AddRig(Panel):
-    """Adds a vdp panel to the TEXT_EDITOR"""
+# Panel that will display the rig menu
+class VDS_PT_RigOptions(Panel):
+    """Adds a vds panel to the TEXT_EDITOR"""
     bl_label = "Rig Options"
     bl_idname = "OBJECT_PT_addRig"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'Physics Vehicles'
+    bl_category = 'Vehicles Sim'
 
     def draw(self, context):
         layout = self.layout
@@ -128,19 +198,18 @@ class VDP_PT_AddRig(Panel):
         # The list of object
         rows = 2
         row = layout.row()
-        row.template_list("VDP_UL_wheels", "", scn, "vdp", scn, "vdp_index", rows=rows)
+        row.template_list("VDS_UL_wheels", "", scn, "vds", scn, "vds_index", rows=rows)
 
         # The buttons that allow control over the list
         col = row.column(align=True)
-        col.operator("vdp.list_action", icon='ZOOM_IN', text="").action = 'ADD'
-        col.operator("vdp.list_action", icon='ZOOM_OUT', text="").action = 'REMOVE'
+        col.operator("vds.list_action", icon='ZOOM_IN', text="").action = 'ADD'
+        col.operator("vds.list_action", icon='ZOOM_OUT', text="").action = 'REMOVE'
         col.separator()
-        col.operator("vdp.list_action", icon='TRIA_UP', text="").action = 'UP'
-        col.operator("vdp.list_action", icon='TRIA_DOWN', text="").action = 'DOWN'
+        col.operator("vds.list_action", icon='TRIA_UP', text="").action = 'UP'
+        col.operator("vds.list_action", icon='TRIA_DOWN', text="").action = 'DOWN'
 
 
-# Panel that will display the rig menu
-class RigOptionsProperties(PropertyGroup):
+class VDS_PG_RigOptionsProperties(PropertyGroup):
     FLWheel : bpy.props.PointerProperty(
         name  ='FL Wheel',
         description = '',
@@ -148,42 +217,22 @@ class RigOptionsProperties(PropertyGroup):
     )
 
 
-class RigOptions(Panel):
-    """Creates a Panel in the Object properties window"""
-    bl_label = "Rig Options"
-    bl_idname = "OBJECT_PT_rigOptions"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = 'Physics Vehicles'
 
-    def draw(self, context):
-        layout = self.layout
-        scene = context.scene
-        obj = context.object
-
-        # rigOptionsPropertiesReference = scene.rigOptionsPropertiesReference_
-
-        # row.operator("object.add_list_item", text="Add List Item")
-
-        layout.label(text="Set the number of axles")
-
-
-        
-#        row = layout.row()
-#        row.template_list("UI_UL_list", "", TestList, "reference_list", TestList, "active_index")
-
-# Panel that will display the vdpizable options for the rig
-class Controls(Panel):
+# Panel that will display the cusomizable options for the rig
+class VDS_PT_Controls(Panel):
     """Creates a Panel in the Object properties window"""
     bl_label = "Controls"
-    bl_idname = "OBJECT_PT_options"
+    bl_idname = "OBJECT_PT_controls"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'Physics Vehicles'
+    bl_category = 'Vehicles Sim'
 
     def draw(self, context):
         layout = self.layout
         obj = context.object
+
+        layout.label(text="Here will be the controls that'll allow")
+        layout.label(text="you to drive the car")
 
         # layout.operator(AddListItemOperator.bl_idname, text="gamer? ong?", icon='SPHERE')
 
@@ -191,7 +240,7 @@ class Controls(Panel):
 #   Collection
 # -------------------------------------------------------------------
 
-class VDP_objectCollection(PropertyGroup):
+class VDS_objectCollection(PropertyGroup):
     #name: StringProperty() -> Instantiated by default
     obj_type: StringProperty()
     obj_id: IntProperty()
@@ -202,12 +251,16 @@ class VDP_objectCollection(PropertyGroup):
 # -------------------------------------------------------------------
 
 classes = (
-    VDP_OT_actions,
-    VDP_UL_wheels,
-    VDP_PT_AddRig,
-    VDP_objectCollection,
-    RigOptionsProperties,
-    RigOptions
+    VDS_OT_actions,
+
+    # Rig
+    VDS_UL_wheels,
+    VDS_PT_RigOptions,
+    VDS_PG_RigOptionsProperties,
+    # Controls
+    VDS_PT_Controls,
+    # COLLECTION
+    VDS_objectCollection,
 )
 
 def register():
@@ -216,17 +269,17 @@ def register():
         register_class(cls)
 
     # Custom scene properties
-    bpy.types.Scene.vdp = CollectionProperty(type=VDP_objectCollection)
-    bpy.types.Scene.vdp_index = IntProperty()
-    bpy.types.Scene.rigOptionsPropertiesReference_ = PointerProperty(type=RigOptionsProperties)
+    bpy.types.Scene.vds = CollectionProperty(type=VDS_objectCollection)
+    bpy.types.Scene.vds_index = IntProperty()
+    bpy.types.Scene.rigOptionsPropertiesReference_ = PointerProperty(type=VDS_PG_RigOptionsProperties)
 
 def unregister():
     from bpy.utils import unregister_class
     for cls in reversed(classes):
         unregister_class(cls)
 
-    del bpy.types.Scene.vdp
-    del bpy.types.Scene.vdp_index
+    del bpy.types.Scene.vds
+    del bpy.types.Scene.vds_index
     del bpy.types.Scene.rigOptionsPropertiesReference_
 
 
@@ -242,9 +295,9 @@ if __name__ == "__main__":
 # from bpy_extras.object_utils import AddObjectHelper, object_data_add
 # from bpy.utils import register_class, unregister_class
 
-# class VDP_OT_actions(Operator):
+# class VDS_OT_actions(Operator):
 #     """Move items up and down, add and remove"""
-#     bl_idname = "vdp.list_action"
+#     bl_idname = "vds.list_action"
 #     bl_label = "List Actions"
 #     bl_description = "Move items up and down, add and remove"
 #     bl_options = {'REGISTER'}
@@ -258,40 +311,40 @@ if __name__ == "__main__":
 
 #     def invoke(self, context, event):
 #         scn = context.scene
-#         idx = scn.vdp_index
+#         idx = scn.vds_index
 
 #         try:
-#             item = scn.vdp[idx]
+#             item = scn.vds[idx]
 #         except IndexError:
 #             pass
 #         else:
-#             if self.action == 'DOWN' and idx < len(scn.vdp) - 1:
-#                 item_next = scn.vdp[idx+1].name
-#                 scn.vdp.move(idx, idx+1)
-#                 scn.vdp_index += 1
-#                 info = 'Item "%s" moved to position %d' % (item.name, scn.vdp_index + 1)
+#             if self.action == 'DOWN' and idx < len(scn.vds) - 1:
+#                 item_next = scn.vds[idx+1].name
+#                 scn.vds.move(idx, idx+1)
+#                 scn.vds_index += 1
+#                 info = 'Item "%s" moved to position %d' % (item.name, scn.vds_index + 1)
 #                 self.report({'INFO'}, info)
 
 #             elif self.action == 'UP' and idx >= 1:
-#                 item_prev = scn.vdp[idx-1].name
-#                 scn.vdp.move(idx, idx-1)
-#                 scn.vdp_index -= 1
-#                 info = 'Item "%s" moved to position %d' % (item.name, scn.vdp_index + 1)
+#                 item_prev = scn.vds[idx-1].name
+#                 scn.vds.move(idx, idx-1)
+#                 scn.vds_index -= 1
+#                 info = 'Item "%s" moved to position %d' % (item.name, scn.vds_index + 1)
 #                 self.report({'INFO'}, info)
 
 #             elif self.action == 'REMOVE':
-#                 info = 'Item "%s" removed from list' % (scn.vdp[idx].name)
-#                 scn.vdp_index -= 1
-#                 scn.vdp.remove(idx)
+#                 info = 'Item "%s" removed from list' % (scn.vds[idx].name)
+#                 scn.vds_index -= 1
+#                 scn.vds.remove(idx)
 #                 self.report({'INFO'}, info)
 
 #         if self.action == 'ADD':
 #             if context.object:
-#                 item = scn.vdp.add()
+#                 item = scn.vds.add()
 #                 item.name = context.object.name
 #                 item.obj_type = context.object.type
-#                 item.obj_id = len(scn.vdp)
-#                 scn.vdp_index = len(scn.vdp)-1
+#                 item.obj_id = len(scn.vds)
+#                 scn.vds_index = len(scn.vds)-1
 #                 info = '"%s" added to list' % (item.name)
 #                 self.report({'INFO'}, info)
 #             else:
@@ -299,13 +352,13 @@ if __name__ == "__main__":
 #         return {"FINISHED"}
 
 
-# class VDP_UL_wheels(UIList):
+# class VDS_UL_wheels(UIList):
 #     def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
 #         split = layout.split(factor=0.3)
 #         split.label(text="Index: %d" % (index))
-#         vdp_icon = "OUTLINER_OB_%s" % item.obj_type
-#         #split.prop(item, "name", text="", emboss=False, translate=False, icon=vdp_icon)
-#         split.label(text=item.name, icon=vdp_icon) # avoids renaming the item by accident
+#         vds_icon = "OUTLINER_OB_%s" % item.obj_type
+#         #split.prop(item, "name", text="", emboss=False, translate=False, icon=vds_icon)
+#         split.label(text=item.name, icon=vds_icon) # avoids renaming the item by accident
 
 #     def invoke(self, context, event):
 #         pass
@@ -326,8 +379,8 @@ if __name__ == "__main__":
 
 # # Declare the classes to be registered, then register and unregister said classes
 # classes = [
-#     VDP_OT_actions,
-#     VDP_UL_wheels,
+#     VDS_OT_actions,
+#     VDS_UL_wheels,
 #     RigOptionsProperties,
 #     RigOptions,
 #     Controls
